3eb64cec9439d32288badff41305541b06c6deee
bh0fer <lebalz@outlook.com>
2026-08-05 17:56:14 +0200

    [tdev] migrate imports to mobx@7 (using @observableRef instead of @observable.ref).

--- website/stores/ToolsStore/index.ts
+++ website/stores/ToolsStore/index.ts
@@ -1,13 +1,13 @@
 import { RootStore } from '@site/src/stores/rootStore';
-import { action, observable } from 'mobx';
+import { action, observable, observableRef } from 'mobx';
 import ClockStore from './ClockStore';
 import LedStore from './LedStore';
 
 type Source = 'text' | 'cipher';
 
 export class ToolsStore {
-    @observable.ref accessor clockStore = new ClockStore();
-    @observable.ref accessor ledStore = new LedStore();
+    @observableRef accessor clockStore = new ClockStore();
+    @observableRef accessor ledStore = new LedStore();
 
     textFieldPlaygrounds = observable.map<string, string>();
 
